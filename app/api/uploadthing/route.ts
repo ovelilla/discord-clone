a727ce923aa742ee7192b2eb6a600e63e1b5542f
Oscar <o.velillaa@gmail.com>
2023-08-27 23:53:27 +0200

    first commit

---
+++ app/api/uploadthing/route.ts
@@ -0,0 +1,6 @@
+import { createNextRouteHandler } from "uploadthing/next";
+import { ourFileRouter } from "@/app/api/uploadthing/core";
+
+export const { GET, POST } = createNextRouteHandler({
+  router: ourFileRouter,
+});
